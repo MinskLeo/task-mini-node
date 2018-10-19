400e202c0f32482d714f47f1b173d448ebfc1b21
DrDesus <shybut69@gmail.com>
2018-10-19 22:33:54 +0300

    Added route /users/:nickname/projects

--- Routes/Users.js
+++ Routes/Users.js
@@ -33,5 +33,23 @@ module.exports = function (dbController, shemas) {
     res.send(ANSWER);
   });
 
+  router.get('/:nickname/projects',async (req,res)=>{
+    const { auth } = req.headers;    
+    let ANSWER = new ResponseObject(403,{},'Forbidden');
+    if(auth) {
+      const verified = await AuthService.verifyAuth(shemas, auth);
+      if (verified) {
+        const {_id} = verified;
+        const projects = await DBService.findProjectsById(shemas, _id);
+        if(projects) {
+          
+          ANSWER = new ResponseObject(200, projects, 'OK');
+        }
+      }
+    }
+
+    res.send(ANSWER);
+  })
+
   return router;
 };
